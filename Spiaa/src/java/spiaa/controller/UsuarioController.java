e456e1ce05674ba393628cbb712cb7d297923462
WilliamDaniel <william.fai.si@gmail.com>
2015-05-22 16:23:59 -0300

    Melhoras no update de usuário (caso o usuário logado faça seu update)

--- Spiaa/src/java/spiaa/controller/UsuarioController.java
+++ Spiaa/src/java/spiaa/controller/UsuarioController.java
@@ -14,122 +14,134 @@ import spiaa.model.entity.Usuario;
 @Controller
 public class UsuarioController {
 
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public ModelAndView login() {
-        ModelAndView mv = new ModelAndView("login/form");
-        return mv;
-    }
+   @RequestMapping(value = "login", method = RequestMethod.GET)
+   public ModelAndView login() {
+      ModelAndView mv = new ModelAndView("login/form");
+      return mv;
+   }
 
-    @RequestMapping(value = "login", method = RequestMethod.POST)
-    public ModelAndView login(String usuario, String senha, HttpSession session) throws Exception {
-        ModelAndView mv = null;
-        try {
-            Usuario usuariologado = ServiceLocator.getBaseUsuarioService()
-                    .login(usuario, senha);
-            session.setAttribute("usuarioLogado", usuariologado);
-            mv = new ModelAndView("redirect:/liraa");
-        } catch (Exception e) {
-            mv = new ModelAndView("erro/erro");
-            mv.addObject("erro", e.getCause());
-        }
-        return mv;
-    }
+   @RequestMapping(value = "login", method = RequestMethod.POST)
+   public ModelAndView login(String usuario, String senha, HttpSession session) throws Exception {
+      ModelAndView mv = null;
+      try {
+         Usuario usuariologado = ServiceLocator.getBaseUsuarioService()
+                 .login(usuario, senha);
+         session.setAttribute("usuarioLogado", usuariologado);
+         mv = new ModelAndView("redirect:/liraa");
+      } catch (Exception e) {
+         mv = new ModelAndView("erro/erro");
+         mv.addObject("erro", e.getCause());
+      }
+      return mv;
+   }
 
-    @RequestMapping(value = "/usuario", method = RequestMethod.GET)
-    public ModelAndView read() throws Exception {
-        ModelAndView mv;
-        try {
-            List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
-                    .readByCriteria(new HashMap<String, Object>());
-            mv = new ModelAndView("usuario/list");
-            mv.addObject("usuario", usuarioList);
-        } catch (Exception ex) {
-            mv = new ModelAndView("erro");
-            mv.addObject("erro", ex);
-        }
-        return mv;
-    }
+   @RequestMapping(value = "/usuario", method = RequestMethod.GET)
+   public ModelAndView read() throws Exception {
+      ModelAndView mv;
+      try {
+         List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
+                 .readByCriteria(new HashMap<String, Object>());
+         mv = new ModelAndView("usuario/list");
+         mv.addObject("usuario", usuarioList);
+      } catch (Exception ex) {
+         mv = new ModelAndView("erro");
+         mv.addObject("erro", ex);
+      }
+      return mv;
+   }
 
-    @RequestMapping(value = "/usuario/novo", method = RequestMethod.GET)
-    public ModelAndView create() throws Exception {
-        ModelAndView mv;
-        try {
-            List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
-                    .readByCriteria(new HashMap<String, Object>());
-            mv = new ModelAndView("usuario/form");
-            mv.addObject("usuarioList", usuarioList);
-        } catch (Exception e) {
-            mv = new ModelAndView("erro/erro");
-            mv.addObject("erro", e.getCause());
-        }
-        return mv;
-    }
+   @RequestMapping(value = "/usuario/novo", method = RequestMethod.GET)
+   public ModelAndView create() throws Exception {
+      ModelAndView mv;
+      try {
+         List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
+                 .readByCriteria(new HashMap<String, Object>());
+         mv = new ModelAndView("usuario/form");
+         mv.addObject("usuarioList", usuarioList);
+      } catch (Exception e) {
+         mv = new ModelAndView("erro/erro");
+         mv.addObject("erro", e.getCause());
+      }
+      return mv;
+   }
 
-    @RequestMapping(value = "/usuario/novo", method = RequestMethod.POST)
-    public ModelAndView create(Usuario usuario) throws Exception {
-        ModelAndView mv;
-        try {
-            ServiceLocator.getBaseUsuarioService().create(usuario);
+   @RequestMapping(value = "/usuario/novo", method = RequestMethod.POST)
+   public ModelAndView create(Usuario usuario) throws Exception {
+      ModelAndView mv;
+      try {
+         ServiceLocator.getBaseUsuarioService().create(usuario);
+         List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
+                 .readByCriteria(new HashMap<String, Object>());
+         mv = new ModelAndView("redirect:/usuario");
+         mv.addObject("usuarioList", usuarioList);
+      } catch (Exception e) {
+         mv = new ModelAndView("erro/erro");
+         mv.addObject("erro", e.getCause());
+      }
+      return mv;
+   }
+
+   @RequestMapping(value = "/usuario/{id}/excluir", method = RequestMethod.GET)
+   public ModelAndView delete(HttpSession session, @PathVariable Long id) throws Exception {
+      Usuario u = (Usuario) session.getAttribute("usuarioLogado");
+      ModelAndView mv;
+      if (!id.equals(u.getId())) {//Não deixa excluir se estiver logado
+         try {
+            ServiceLocator.getBaseUsuarioService().delete(id);
             List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
                     .readByCriteria(new HashMap<String, Object>());
             mv = new ModelAndView("redirect:/usuario");
             mv.addObject("usuarioList", usuarioList);
-        } catch (Exception e) {
+         } catch (Exception e) {
             mv = new ModelAndView("erro/erro");
             mv.addObject("erro", e.getCause());
-        }
-        return mv;
-    }
+         }
+         return mv;
+      }
+      return new ModelAndView("redirect:/usuario");
+   }
 
-    @RequestMapping(value = "/usuario/{id}/excluir", method = RequestMethod.GET)
-    public ModelAndView delete(HttpSession session, @PathVariable Long id) throws Exception {
-        Usuario u = (Usuario) session.getAttribute("usuarioLogado");
-        ModelAndView mv;
-        if (!id.equals(u.getId())) {
-            try {
-                ServiceLocator.getBaseUsuarioService().delete(id);
-                List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
-                        .readByCriteria(new HashMap<String, Object>());
-                mv = new ModelAndView("redirect:/usuario");
-                mv.addObject("usuarioList", usuarioList);
-            } catch (Exception e) {
-                mv = new ModelAndView("erro/erro");
-                mv.addObject("erro", e.getCause());
+   @RequestMapping(value = "/usuario/{id}/atualizar", method = RequestMethod.GET)
+   public ModelAndView update(@PathVariable Long id) throws Exception {
+      Usuario user = ServiceLocator.getBaseUsuarioService().readById(id);
+      ModelAndView mv = new ModelAndView("usuario/form");
+      mv.addObject("user", user);
+      return mv;
+   }
+
+   @RequestMapping(value = "/usuario/{id}/atualizar", method = RequestMethod.POST)
+   public ModelAndView update(HttpSession session, Usuario usuario) throws Exception {
+      ModelAndView mv;
+      try {
+         ServiceLocator.getBaseUsuarioService().update(usuario);
+         List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
+                 .readByCriteria(new HashMap<String, Object>());
+         Usuario usuarioLogado = (Usuario)session.getAttribute("usuarioLogado");
+         for(Usuario u : usuarioList){ //Atualiza os dados do usuário logado
+            if(u.getId().equals(usuarioLogado.getId())){
+               session.setAttribute("usuarioLogado", u);
             }
-            return mv;
-        }
-        return new ModelAndView("redirect:/usuario");
-    }
+         }
+         mv = new ModelAndView("redirect:/usuario");
+         mv.addObject("usuarioList", usuarioList);
+      } catch (Exception ex) {
+         mv = new ModelAndView("erro");
+         mv.addObject("errp", ex);
+      }
+      return mv;
+   }
 
-    @RequestMapping(value = "/usuario/{id}/atualizar", method = RequestMethod.GET)
-    public ModelAndView update(@PathVariable Long id) throws Exception {
-        Usuario user = ServiceLocator.getBaseUsuarioService().readById(id);
-        ModelAndView mv = new ModelAndView("usuario/form");
-        mv.addObject("user", user);
-        return mv;
-    }
+   @RequestMapping(value = "/usuario/{id}/detalhes", method = RequestMethod.GET)
+   public ModelAndView show(@PathVariable Long id) throws Exception {
+      ModelAndView mv = new ModelAndView("usuario/list");
+      Usuario usuario = ServiceLocator.getBaseUsuarioService().readById(id);
+      mv.addObject("usuario", usuario);
+      return mv;
+   }
 
-    @RequestMapping(value = "/usuario/{id}/atualizar", method = RequestMethod.POST)
-    public ModelAndView update(HttpSession session, Usuario usuario) throws Exception {
-        ServiceLocator.getBaseUsuarioService().update(usuario);
-        session.removeAttribute("usuarioList");
-        List<Usuario> usuarioList = ServiceLocator.getBaseUsuarioService()
-                .readByCriteria(new HashMap<String, Object>());
-        session.setAttribute("usuarioList", usuarioList);
-        return new ModelAndView("redirect:/usuario");
-    }
-
-    @RequestMapping(value = "/usuario/{id}/detalhes", method = RequestMethod.GET)
-    public ModelAndView show(@PathVariable Long id) throws Exception {
-        ModelAndView mv = new ModelAndView("usuario/list");
-        Usuario usuario = ServiceLocator.getBaseUsuarioService().readById(id);
-        mv.addObject("usuario", usuario);
-        return mv;
-    }
-
-    @RequestMapping("logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:login";
-    }
+   @RequestMapping("logout")
+   public String logout(HttpSession session) {
+      session.invalidate();
+      return "redirect:login";
+   }
 }
