7c7258d910b781ff0ebd44e26779cb9636063d17
Felipe Pereira de souza <felipepdsouza@gmail.com>
2015-06-25 07:36:05 -0300

    sincronizar
    importar/exportar

---
+++ spiaa-model/src/spiaa/model/service/ImportarDadosService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spiaa.model.service;
+
+import java.util.List;
+import spiaa.model.base.service.BaseImportarDadosService;
+import spiaa.model.entity.ImportarDados;
+
+/**
+ *
+ * @author Felipe de Souza
+ */
+public class ImportarDadosService implements BaseImportarDadosService{
+
+    @Override
+    public List<ImportarDados> readData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
